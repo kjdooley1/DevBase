11742b3a941416a85ad44d745826aa20adea914f
Cam-Kelly <cameronkelly249@gmail.com>
2022-11-26 15:24:00 -0500

    updates

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,10 +1,22 @@
+import * as types from '../constants/actionTypes'
+
+
 //create user
 export const addMessageActionCreator = message => ({
     type: types.ADD_MESSAGE,
     payload: message,
   });
 
+//load users
+export const loadUsersActionCreator = () => (
+  fetch('/users')
+  .then(data => data.json())
+  .then({
+    type: types.LOAD_USERS,
+    payload: data
+  })
 
+);
 
 
 //send a message
